7cb42eca229f63526debdf2eed6fd79539538a9c
Kirk Bridger <kirk@thebside.ca>
2022-12-03 19:24:03 -0800

    iUpload after tinkering

--- __init__.py
+++ __init__.py
@@ -5,10 +5,20 @@ from mycroft.util import LOG
 track_dict = {
     'groovesalad': 'https://ice4.somafm.com/groovesalad-128-mp3',
     'groove salad': 'https://ice4.somafm.com/groovesalad-128-mp3',
-    'suburbs of goa': 'https://ice4.somafm.com/suburbsofgoa-128-mp3'
+    'suburbs of goa': 'https://ice4.somafm.com/suburbsofgoa-128-mp3',
+    'test': 'https://cbc.mc.tritondigital.com/CBC_HOURLYNEWS_P/media/hourlynews/hourlynews-ttA7xpoG-20221201.mp3'
 }
 
+def new_func():
+    LOG.info("this is outside the class")
+
+
 class SomafmPlayer(CommonPlaySkill):
+
+    def initialize(self):
+        self.log.info("the skill is loaded!")
+#        new_func()
+
     def CPS_match_query_phrase(self, phrase):
         """ This method responds whether the skill can play the input phrase.
 
@@ -36,12 +46,27 @@ class SomafmPlayer(CommonPlaySkill):
         """
         # Retrieve the track url from the data
         url = data['track']
-        self.log.info(f"Playing from: {url}")
-        self.log.exception("This is an error.")
-        self.speak_dialog("Playing Soma FM")
+        #self.log.info(f"Playing from: {url}")
+        #self.log.exception("This is an error.")
+        # Let's output this data to see what came in
+        self.log.info("Let's dump some data to see what's what.")
+        self.log.info(f"self: {self}")
+        self.log.info(f"phrase: {phrase}")
+        self.log.info(f"data: {data}")
+        self.log.info(f"url: {url}")
+        #self.speak_dialog("Playing Soma FM")
         # Send url to audioservice to start playback
-        self.audioservice.play(url) 
-        #self.CPS_play(["url"])
+        #self.audioservice.play(url) 
+        self.CPS_play(url)
+#        try:
+#            print(new_var)
+#        except NameError:
+#            self.log.exception("This is an exception")
+        self.log.info(f"That should have worked to play: {url}")
+#        self.log.debug("This is debug level log")
+#        self.log.info("Let's just play this URL directly")
+#        self.CPS_play("https://ice4.somafm.com/groovesalad-128-mp3")
+#        self.log.info("That should have played")
 
 
 def create_skill():
